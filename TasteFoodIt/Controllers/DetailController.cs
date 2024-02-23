7b17aafcba1445a5314a025207a246b7e61f8b89
Emre Keskiner <emrekeskiner@gmail.com>
2024-02-23 09:17:42 +0300

    DetailLayout için örnek Contact Sayfası oluşturuldu

---
+++ TasteFoodIt/Controllers/DetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TasteFoodIt.Controllers
+{
+    public class DetailController : Controller
+    {
+        // GET: Detail
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
