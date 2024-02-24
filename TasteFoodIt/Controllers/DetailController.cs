50bf7b970bca916e1539c488a06cd4a0ef0fe477
Emre Keskiner <emrekeskiner@gmail.com>
2024-02-24 09:27:58 +0300

    Detail sayfa tasarımları bitti

--- TasteFoodIt/Controllers/DetailController.cs
+++ TasteFoodIt/Controllers/DetailController.cs
@@ -37,5 +37,10 @@ namespace TasteFoodIt.Controllers
         {
             return View();
         }
+
+        public ActionResult Reservation()
+        {
+            return View();
+        }
     }
 }
